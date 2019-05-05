0e9e00457438d9ceda07aa7b9821c53d547cbcdd
Jaikishan Jeshnani <jeshnanijai@gmail.com>
2019-05-05 16:25:11 +0530

    Configure transport for winston logger

--- config/logger.js
+++ config/logger.js
@@ -1,4 +1,4 @@
-const { createLogger, format } = require('winston');
+const { createLogger, format, transports } = require('winston');
 
 const logger = createLogger({
   level: 'info',
@@ -7,7 +7,15 @@ const logger = createLogger({
       format: 'YYYY-MM-DD HH:mm:ss:SSS'
     }),
     format.json()
-  )
+  ),
+  transports: [
+    //
+    // - Write to all logs with level `info` and below to `combined.log`
+    // - Write all logs error (and below) to `error.log`.
+    //
+    new transports.File({ filename: 'error.log', level: 'error' }),
+    new transports.File({ filename: 'combined.log' })
+  ]
 });
 
 module.export = logger;
