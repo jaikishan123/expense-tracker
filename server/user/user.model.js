a2f4be6f525fca38f5e31417c3ea8b92bc36659b
Jaikishan Jeshnani <jeshnanijai@gmail.com>
2019-05-12 04:52:57 +0530

    Add pre-save middleware to hash the password while storing

--- server/user/user.model.js
+++ server/user/user.model.js
@@ -1,9 +1,31 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
+const SALT_WORK_FACTOR = 10;
+/**
+ * User Schema
+ */
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   createdAt: { type: Date, default: Date.now }
+});
+
+/**
+ * - pre-save hooks
+ */
+
+// The following pre-save hook will hash the user password before saving
+UserSchema.pre('save', function hashPassword(next) {
+  if (!this.isModified('password')) return next();
+  bcrypt
+    .hash(this.password, SALT_WORK_FACTOR)
+    .then(function setHash(hash) {
+      this.password = hash;
+      return next();
+    })
+    .catch(next);
+  return next();
 });
 
 module.exports = mongoose.model('User', UserSchema);
