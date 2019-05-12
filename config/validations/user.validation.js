7ed784639a801bb1e007f1566468615aee025b44
Jaikishan Jeshnani <jeshnanijai@gmail.com>
2019-05-12 22:38:45 +0530

    Add validations for update user

--- config/validations/user.validation.js
+++ config/validations/user.validation.js
@@ -1,11 +1,22 @@
 const Joi = require('@hapi/joi');
 
 module.exports = {
-  // GET /v1/users
+  // POST /api/users
   createUser: {
     body: {
       username: Joi.string().required(),
       password: Joi.string().required()
+    }
+  }, // UPDATE /api/users/:userId
+  updateUser: {
+    body: {
+      username: Joi.string(),
+      password: Joi.string()
+    },
+    params: {
+      userId: Joi.string()
+        .hex()
+        .required()
     }
   }
 };
