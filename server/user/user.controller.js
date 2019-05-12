93c9d5a6dad9c417eaee0faea289f52051f5a323
Jaikishan Jeshnani <jeshnanijai@gmail.com>
2019-05-12 18:41:14 +0530

    Add get all users with pagination options

---
+++ server/user/user.controller.js
@@ -0,0 +1,16 @@
+const User = require('./user.model');
+
+/**
+ * Get user list.
+ * @property {number} req.query.skip - Number of users to be skipped.
+ * @property {number} req.query.limit - Limit number of users to be returned.
+ * @returns {User[]}
+ */
+function list(req, res, next) {
+  const { limit = 50, skip = 0 } = req.query;
+  User.list({ limit, skip })
+    .then(users => res.json(users))
+    .catch(e => next(e));
+}
+
+module.exports = { list };
