087f6c2fa15749e334960450540b36674717ce1d
Jaikishan Jeshnani <jeshnanijai@gmail.com>
2019-08-04 18:49:27 +0530

    Add Login function
    The login function expects a username and password in request body and will return JWT if username and password is valid

---
+++ server/auth/auth.controller.js
@@ -0,0 +1,50 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const httpStatus = require('http-status');
+const APIError = require('../utils/APIErrors.js');
+const userModel = require('../user/user.model');
+const config = require('../../config/config');
+
+/**
+ * Login a user and return JWT token
+ * @property {string} req.body.username - The username of user.
+ * @property {string} req.body.password - The password of user.
+ * @returns {token, username: req.body.username} - A json containing token and username
+ */
+function login(req, res, next) {
+  userModel
+    .findOne({ username: req.body.username })
+    .exec()
+    .then(user => {
+      if (user) {
+        return bcrypt.compare(req.bod.password, user.password);
+      }
+      const error = new APIError(
+        'username does not exists',
+        httpStatus.UNAUTHORIZED,
+        true
+      );
+      return Promise.reject(error);
+    })
+    .then(response => {
+      if (response) {
+        const token = jwt.sign(
+          { username: req.body.username },
+          config.jwtSecret,
+          { expiresIn: '24h' }
+        );
+        return res.json({ token, username: req.body.username });
+      }
+      const error = new APIError(
+        'Password does not match',
+        httpStatus.UNAUTHORIZED,
+        true
+      );
+      return Promise.reject(error);
+    })
+    .catch(error => {
+      next(error);
+    });
+}
+
+module.exports = { login };
