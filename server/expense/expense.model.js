05ece2e8f9b4570b479b23dc1f3977214c4ae22e
Jaikishan Jeshnani <jeshnanijai@gmail.com>
2019-08-05 02:45:52 +0530

    Add expense mongoose schema

---
+++ server/expense/expense.model.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+/**
+ * Expense Schema
+ */
+const ExpenseSchema = new mongoose.Schema({
+  amount: {
+    type: Number,
+    required: true
+  },
+  category: {
+    type: mongoose.schema.Types.ObjectId,
+    lowercase: true,
+    required: true
+  },
+  coordinates: [Number],
+  date: {
+    type: Date,
+    defaultValue: Date.now()
+  },
+  description: {
+    type: String,
+    lowercase: true
+  },
+  type: {
+    type: String,
+    enum: ['expense', 'income'],
+    required: true
+  },
+  user: {
+    type: mongoose.schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+});
+
+module.exports = mongoose.model('expense', ExpenseSchema);
