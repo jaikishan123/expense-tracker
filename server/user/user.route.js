7b8671d55f4be2d6e1ffb0b39b905147301d099a
Jaikishan Jeshnani <jeshnanijai@gmail.com>
2019-08-04 23:39:28 +0530

    Add middleware for user authentication

--- server/user/user.route.js
+++ server/user/user.route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const validate = require('express-validation');
 const userCtrl = require('./user.controller.js');
 const validation = require('../../config/validations/user.validation.js');
+const authenticate = require('../middlewares/authenticate.js');
 
 const router = express.Router();
 /**
@@ -19,12 +20,16 @@ router
 router
   .route('/:userId')
   /** GET /api/users/:userId - Get user */
-  .get(userCtrl.get)
+  .get(authenticate.authenticateUser, userCtrl.get)
 
   /** PUT /api/users/:userId - Replace user */
-  .put(validate(validation.replaceUser), userCtrl.replace)
+  .put(
+    authenticate.authenticateUser,
+    validate(validation.replaceUser),
+    userCtrl.replace
+  )
 
   /** DELETE /api/users/:userId - Delete user */
-  .delete(userCtrl.remove);
+  .delete(authenticate.authenticateUser, userCtrl.remove);
 
 module.exports = router;
