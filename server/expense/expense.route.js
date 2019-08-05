7cc953ffabee7d25f952c2a0276de0b4766f58c2
Jaikishan Jeshnani <jeshnanijai@gmail.com>
2019-08-05 17:23:01 +0530

    Add expense route

---
+++ server/expense/expense.route.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const validate = require('express-validation');
+const expenseCtrl = require('./expense.controller.js');
+const validation = require('../../config/validations/expense.validation.js');
+const authenticate = require('../middlewares/authenticate.js');
+
+const router = express.Router();
+
+/**
+ * Load expense for a user when API with userId and expenseId route parameter is hit
+ */
+router.param('expenseId', expenseCtrl.load);
+
+router
+  .route('/:expenseId')
+  /** GET /api/users/:userId/expenses/:expenseId - Get expense of a user */
+  .get(authenticate.authenticateUser, expenseCtrl.getOne)
+
+  /** PUT /api/users/:userId/expenses/:expenseId - Replace expense of the user */
+  .put(
+    authenticate.authenticateUser,
+    validate(validation.replaceExpense),
+    expenseCtrl.replace
+  )
+
+  /** DELETE /api/users/:userId/expenses/:expenseId - Delete expense of a user */
+  .delete(authenticate.authenticateUser, expenseCtrl.remove);
+module.exports = router;
