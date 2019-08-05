d4359e576517fe090b5aefea6edaffcbb6b1efc7
Jaikishan Jeshnani <jeshnanijai@gmail.com>
2019-08-05 18:11:52 +0530

    Add static method for get an expense
    The reason for the check of expense.userId === userId is lets say if a user accidentally enters a random expenseId but this expenseId belongs to some other user, this can cause some complications such as a user cannot view expense of other users etc

--- server/expense/expense.model.js
+++ server/expense/expense.model.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const httpStatus = require('http-status');
+const APIError = require('../utils/APIErrors.js');
+
 /**
  * Expense Schema
  */
@@ -33,4 +36,26 @@ const ExpenseSchema = new mongoose.Schema({
   }
 });
 
+ExpenseSchema.statics = {
+  /**
+   * Get expense of a user
+   * @param {ObjectId} userId - The objectId of the user.
+   * @param {ObjectId} expenseId - The objectId of the expense.
+   * @returns {Promise<User, APIError>}
+   */
+  get(userId, expenseId) {
+    return this.findById(expenseId)
+      .exec()
+      .then(expense => {
+        if (expense && expense.userId === userId) {
+          return expense;
+        }
+        const err = new APIError(
+          'No such expense exists!',
+          httpStatus.NOT_FOUND
+        );
+        return Promise.reject(err);
+      });
+  }
+};
 module.exports = mongoose.model('expense', ExpenseSchema);
